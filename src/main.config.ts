266db85b656afde8a3405c17855cd9a9fa430acd
Piotr <piotr.przybytniewski@gmail.com>
2024-08-31 11:37:41 +0200

    build: app reusable main config file

---
+++ src/main.config.ts
@@ -0,0 +1,29 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function mainConfig(app: INestApplication) {
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(new Reflector()));
+
+  app.enableCors({
+    origin: [process.env.CLIENT_HOST],
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('ScheduleMyPost API')
+    .setDescription(
+      'ScheduleMyPost API allows scheduling posts to Reddit at the best time to get more engagement!',
+    )
+    .setVersion('1.0')
+    .addTag('posts')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
